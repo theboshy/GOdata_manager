d59bba97eddcd38b3dedd4c5fb2c5b03704cb351
Peter Lobo <akumaokami10@gmail.com>
2018-03-20 17:31:13 -0500

    orm (xorm) integration with mysql,fixed sintax

--- src/DataManagerORM.go
+++ src/DataManagerORM.go
@@ -12,22 +12,25 @@ import (
 var engine *xorm.Engine
 
 func main() {
-	type radicacion struct {
-		numeroRadicacion string `xorm:"varchar(45) not null unique 'numero_radicacion'"`
-		fechaRadicacion  time.Time `xorm:"TIMESTAMP 'fecha_radicacion'"`
-		fechaDocumento string `xorm:"<-"`
-		asunto string `xorm:"VARCHAR(60) 'asunto'"`
-		idRemitente string `xorm:"varchar(45) 'id_remitente'"`
-		pdfidPdf int16 `xorm:"INT(11) 'pdf_idPDF'"`
-		tiempoRespuesta time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'"`
+	type Radicacion struct {
+		NumeroRadicacion string `xorm:"varchar(45) primary key not null unique 'numero_radicacion'"`
+		FechaRadicacion  time.Time `xorm:"TIMESTAMP 'fecha_radicacion'"`
+		FechaDocumento string `xorm:"<-"`
+		Asunto string `xorm:"VARCHAR(60) 'asunto'"`
+		IdRemitente string `xorm:"varchar(45) 'id_remitente'"`
+		PdfidPdf int16 `xorm:"INT(11) 'pdf_idPDF'"`
+		TiempoRespuesta time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'"`
 	}
 
 	var errGe error
 	engine, errGe = xorm.NewEngine("mysql", "root:lexco123@tcp(127.0.0.1:3306)/sgdea")
 	engine.SetMapper(core.SameMapper{})
-	engine.Sync2(new(radicacion))
+	engine.Sync2(new(Radicacion))
 	fmt.Print(errGe)
 
+	radci := Radicacion{NumeroRadicacion :"REC540"}
+	exist,errror :=engine.Get(&radci)
+	fmt.Print(exist,errror)
 	result,errrrr := engine.QueryString("select *  from radicacion");
 
 //ff := engine.Table("radicacion").Select("radicacion.*");
