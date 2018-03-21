0c656945e6617543dae459cf24b86be19ed9eaa1
jcamilo97 <j.caviedes@udla.edu.co>
2018-03-20 19:19:36 -0500

    Utilizacion de fun Get y Find

--- src/DataManagerORM.go
+++ src/DataManagerORM.go
@@ -1,64 +1,61 @@
 package main
 
 import (
-	"github.com/go-xorm/xorm"
-	"time"
 	"fmt"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/core"
-
+	"github.com/go-xorm/xorm"
 )
+
+// Radicacion describes a radicacion
+type Radicacion struct {
+	NumeroRadicacion string    `xorm:"varchar(45) primary key not null unique 'numero_radicacion'"`
+	FechaRadicacion  string    `xorm:"<-"`
+	FechaDocumento   string    `xorm:"<-"`
+	Asunto           string    `xorm:"VARCHAR(60) not null 'asunto'"`
+	IDRemitente      string    `xorm:"varchar(45) not null 'id_remitente'"`
+	PdfidPdf         int16     `xorm:"INT(11) 'pdf_idPDF'"`
+	TiempoRespuesta  time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'"`
+}
 
 var engine *xorm.Engine
 
 func main() {
-	type Radicacion struct {
-		NumeroRadicacion string `xorm:"varchar(45) primary key not null unique 'numero_radicacion'"`
-		FechaRadicacion  time.Time `xorm:"TIMESTAMP 'fecha_radicacion'"`
-		FechaDocumento string `xorm:"<-"`
-		Asunto string `xorm:"VARCHAR(60) 'asunto'"`
-		IdRemitente string `xorm:"varchar(45) 'id_remitente'"`
-		PdfidPdf int16 `xorm:"INT(11) 'pdf_idPDF'"`
-		TiempoRespuesta time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'"`
-	}
 
-	var errGe error
-	engine, errGe = xorm.NewEngine("mysql", "root:lexco123@tcp(127.0.0.1:3306)/sgdea")
+	var err error
+	engine, err = xorm.NewEngine("mysql", "root:lexco123@tcp(192.168.20.180:3306)/sgdea")
 	engine.SetMapper(core.SameMapper{})
 	engine.Sync2(new(Radicacion))
-	fmt.Print(errGe)
+	fmt.Print(err)
 
-	radci := Radicacion{NumeroRadicacion :"REC540"}
-	exist,errror :=engine.Get(&radci)
-	fmt.Print(exist,errror)
-	result,errrrr := engine.QueryString("select *  from radicacion");
+	//radci := []Radicacion{NumeroRadicacion: "REC540"}
+	var radci []Radicacion
 
-//ff := engine.Table("radicacion").Select("radicacion.*");
-
-for i:=0;i<len(result);i++{
-	fmt.Print("posicion ",i,"[");
-	for k, v := range result[i] {
-		fmt.Printf("nombreColumna[%s] valor[%s]\n", k, v )
-	}
-	fmt.Println("]");
-}
-
-
-	fmt.Print(errrrr)
-
-
-	/*session := engine.NewSession()
-	defer session.Close()
-
-	err := session.Begin()
-
-	user1 := radicacion{numeroRadicacion: "REC591"}
-	has, err  := engine.Get(&user1)
-fmt.Print(has)
-	err = session.Commit()
+	err = engine.Find(&radci)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 		return
-	}*/
+	}
 
+	fmt.Println("radicacionesAll:", radci)
+
+	radicacion := new(Radicacion)
+	has, err := engine.Get(radicacion)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nradicacionGET:", has, radicacion)
+
+	var radicacion1 []Radicacion
+	err1 := engine.Table("radicacion").Where("numero_radicacion = ?", "REC540").Find(&radicacion1)
+	if err1 != nil {
+		fmt.Println("errorDB", err1)
+		return
+	}
+
+	fmt.Println("\nradicacionWhereGET:", radicacion1)
 }
