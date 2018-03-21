5195bbbc4c87b95a6cb76a6a899d4add60dd628e
Peter Lobo <akumaokami10@gmail.com>
2018-03-21 12:10:22 -0500

    json sintax for struct in xorm database returns

--- src/DataManagerORM.go
+++ src/DataManagerORM.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/go-xorm/core"
+	 _"github.com/go-sql-driver/mysql"
+	core "github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
+	"encoding/json"
 )
 
 // Radicacion describes a radicacion
 type Radicacion struct {
-	NumeroRadicacion string    `xorm:"varchar(45) primary key not null unique 'numero_radicacion'"`
+	NumeroRadicacion string    `xorm:"varchar(45) primary key not null unique 'numero_radicacion'"` //`json:"numeroRadicacion"`
 	FechaRadicacion  string    `xorm:"<-"`
 	FechaDocumento   string    `xorm:"<-"`
 	Asunto           string    `xorm:"VARCHAR(60) not null 'asunto'"`
@@ -19,6 +19,18 @@ type Radicacion struct {
 	PdfidPdf         int16     `xorm:"INT(11) 'pdf_idPDF'"`
 	TiempoRespuesta  time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'"`
 }
+
+/**
+* struc parse like json
+type Radicacionjson struct {
+	NumeroRadicacion string    `xorm:"varchar(45) primary key not null unique 'numero_radicacion'" json:"numeroRadicacion"`
+	FechaRadicacion  string    `xorm:"<-" json:"fechaRadicacion"`
+	FechaDocumento   string    `xorm:"<-" json:"fechaDocuemento"`
+	Asunto           string    `xorm:"VARCHAR(60) not null 'asunto'" json:"asunto"`
+	IDRemitente      string    `xorm:"varchar(45) not null 'id_remitente'" json:"idRemitente"`
+	PdfidPdf         int16     `xorm:"INT(11) 'pdf_idPDF'" json:"pdfIdPdf"`
+	TiempoRespuesta  time.Time `xorm:"TIMESTAMP 'tiempo_respuesta'" json:"tiempoRespuesta"`
+}*/
 
 var engine *xorm.Engine
 
@@ -29,6 +41,8 @@ func main() {
 	engine.SetMapper(core.SameMapper{})
 	engine.Sync2(new(Radicacion))
 	fmt.Print(err)
+
+
 
 	//radci := []Radicacion{NumeroRadicacion: "REC540"}
 	var radci []Radicacion
@@ -51,6 +65,7 @@ func main() {
 	fmt.Println("\nradicacionGET:", has, radicacion)
 
 	var radicacion1 []Radicacion
+	//var radicacion2 []Radicacionjson
 	err1 := engine.Table("radicacion").Where("numero_radicacion = ?", "REC540").Find(&radicacion1)
 	if err1 != nil {
 		fmt.Println("errorDB", err1)
@@ -58,4 +73,11 @@ func main() {
 	}
 
 	fmt.Println("\nradicacionWhereGET:", radicacion1)
+
+	b, err := json.Marshal(radicacion1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
